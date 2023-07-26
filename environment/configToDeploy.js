316f0302f096d7fafdba7d377e0e722c9921f0c8
moises <angular.moises82@gmail.com>
2023-07-26 06:39:42 -0300

    me faltaba arreglar el archivo de configuracion a deploy para que cargue las variables de entorno en el codigo

--- environment/configToDeploy.js
+++ environment/configToDeploy.js
@@ -1,6 +1,9 @@
 const configToDeploy = {
 	USER_HOTMAIL_SMTP : process.env.USER_HOTMAIL_SMTP,
-	PASS_HOTMAIL_SMTP : process.env.PASS_HOTMAIL_SMTP
+	PASS_HOTMAIL_SMTP : process.env.PASS_HOTMAIL_SMTP,
+	USER_DATABASE : process.env.USER_DATABA,
+	PASSWORD_DATABASE : process.env.PASSWORD_DATABASE,
+	NAME_DATABASE : process.env.NAME_DATABASE,
 }
 
 
