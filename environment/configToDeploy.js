2f8187c89de25e0b1e20935bd2ea31fb39c00ce7
moises <angular.moises82@gmail.com>
2023-07-26 07:21:54 -0300

    arregle otro error del tema de archivos de configuracion de variables de entorno

--- environment/configToDeploy.js
+++ environment/configToDeploy.js
@@ -1,7 +1,7 @@
 const configToDeploy = {
 	USER_HOTMAIL_SMTP : process.env.USER_HOTMAIL_SMTP,
 	PASS_HOTMAIL_SMTP : process.env.PASS_HOTMAIL_SMTP,
-	USER_DATABASE : process.env.USER_DATABA,
+	USER_DATABASE : process.env.USER_DATABASE,
 	PASSWORD_DATABASE : process.env.PASSWORD_DATABASE,
 	NAME_DATABASE : process.env.NAME_DATABASE,
 }
